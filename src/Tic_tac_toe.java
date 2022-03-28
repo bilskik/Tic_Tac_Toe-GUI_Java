9a21c7393d53e1ed4cfc0a36ce5bd21b918d7fe9
kamil <kbilski77@gmail.com>
2022-03-28 23:31:47 +0200

    adding iswinnner function

--- src/Tic_tac_toe.java
+++ src/Tic_tac_toe.java
@@ -3,13 +3,21 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class Tic_tac_toe extends JFrame implements ActionListener {
+public class Tic_tac_toe implements ActionListener {
     JFrame frame;
-    JButton but_1,but_2,but_3,but_4,but_5,but_6,but_7,but_8,but_9;
+    JButton []button = new JButton[9];
     ImageIcon X,O;
+    JPanel panel;
     int [] checker = new int[9];
+    boolean ply_1,ply_2;
+    char[]map = new char[9];
+    char mark_x = 'X';
+    char mark_o = 'O';
 
     Tic_tac_toe() {
+
+        ply_1 = true;
+        ply_2 = false;
         X = new ImageIcon("X_1.png");
         O = new ImageIcon("O_1.png");
         Image scale_O = O.getImage().getScaledInstance(200,200,100);
@@ -18,84 +26,93 @@ public class Tic_tac_toe extends JFrame implements ActionListener {
         frame.setTitle("Tic_tac_toe");
         frame.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE));
         frame.setSize(600,600);
-        //this.init();
+        for(int i=0; i<9; i++) {
+            button[i] = new JButton();
+            button[i].addActionListener(this);
+        }
+        panel = new JPanel();
+        panel.setBounds(0,0,600,560);
+        panel.setLayout(new GridLayout(3,3,0,0));
+        for(int i=0; i<9; i++) {
+            panel.add(button[i]);
+        }
 
-        but_1 = new JButton();
-        but_1.setBounds(0,0,200,200);
-        but_1.addActionListener(this);
-        frame.add(but_1);
-        but_2 = new JButton();
-        but_2.setBounds(200,0,200,200);
-        but_2.addActionListener(this);
-        frame.add(but_2);
-        but_3 = new JButton();
-        but_3.setBounds(400,0,200,200);
-        frame.add(but_3);
-        but_4 = new JButton();
-        but_4.setBounds(0,200,200,200);
-        frame.add(but_4);
-        but_5 = new JButton();
-        but_5.setBounds(0,400,200,200);
-        frame.add(but_5);
-        but_6 = new JButton();
-        but_6.setBounds(200,200,200,200);
-        frame.add(but_6);
-        but_7 = new JButton();
-        but_7.setBounds(400,200,200,200);
-        frame.add(but_7);
-        but_8 = new JButton();
-        but_8.setBounds(200,400,200,200);
-        frame.add(but_8);
-        but_9 = new JButton();
-        but_9.setBounds(400,400,200,200);
-        frame.add(but_9);
+        if(iswinner(mark_x) == 1)
+            System.out.println("ply1 wygral");
+        if(iswinner(mark_o) == 1)
+            System.out.println("pl2 wygral");
 
+        frame.add(panel);
         frame.setLayout(null);
         frame.setVisible(true);
     }
 
-    public void actionPerformed(ActionEvent a) {
-        if(a.getSource() == but_1) {
-            System.out.println(checker[0]);
-            if(checker[0] == 1) {
-                but_1.setIcon(X);
-                checker[0] = 0;
-                but_1.setEnabled(false);
-            }
-        }
-        else if(a.getSource() == but_2) {
-            if(checker[1] == 1) {
-                but_1.setIcon(X);
-                checker[0] = 0;
-                but_1.setEnabled(false);
-            }
-        }
-        else if(a.getSource() == but_3) {
+    public void actionPerformed(ActionEvent e) {
 
-        }
-        else if(a.getSource() == but_4) {
+      for(int i=0; i<9; i++) {
+          if(e.getSource() == button[i]) {
+              if(checker[i] == 1 && ply_1 == true) {
+                  button[i].setIcon(X);
+                  map[i] = 'X';
+                  checker[i] = 0;
+                  ply_1 = false;
+                  ply_2 = true;
+              }
+              else if(checker[i] == 1 && ply_2 == true) {
+                  button[i].setIcon(O);
+                  map[i] = 'O';
+                  checker[i] = 0;
+                  ply_1 = true;
+                  ply_2 = false;
+              }
+          }
+      }
 
-        }
-        else if(a.getSource() == but_5) {
-
-        }
-        else if(a.getSource() == but_6) {
-
-        }
-        else if(a.getSource() == but_7) {
-
-        }
-        else if(a.getSource() == but_8) {
-
-        }
-        else if(a.getSource() == but_9) {
-
-        }
 
     }
     public static void main(String[] args) {
+
         Tic_tac_toe game = new Tic_tac_toe();
         game.init();
+
+
+    }
+    private int iswinner(char mark) {
+        System.out.println("pl2 wygral");
+        int row = 0;
+        int col = 0;
+        int dia_1 = 0;
+        int dia_2 = 0;
+        int j = 0;
+        for(int i=0; i<9; i++) {
+
+            if(i == 3 || i == 6) {
+                row = 0;
+                col = 0;
+                j = 0;
+            }
+
+            if(map[j] == mark)
+                col++;
+            if(map[i] == mark)
+                row++;
+            if((map[i] == mark) && (i == 0 || i == 4 || i== 8))
+                dia_1++;
+            if((map[i] == mark) && (i == 2 || i == 4 || i == 6))
+                dia_2++;
+
+            if(row == 3)
+                return 1;
+            if(dia_1 == 3)
+                return 1;
+            if(dia_2 == 3)
+                return 1;
+            if(col == 3)
+                return 1;
+            j+=3;
+
+        }
+        return 0;
     }
     private void init() {
         for(int i=0; i<9; i++) {
